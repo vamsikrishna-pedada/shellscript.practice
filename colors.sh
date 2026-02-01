29c56c5b77d99d9298837c1e2ddcb3fb701ac429
Vamsi <vamsikrishnapedada19@gmail.com>
2026-02-01 22:17:02 +0530

    new file addded

--- colors.sh
+++ colors.sh
@@ -5,4 +5,4 @@ G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
-echo -e " $R Hello world, $Y color printed in yellow, $B printed in blue color"
+echo -e " $R Hello world $N, $Y color printed in yellow $N, $B printed in blue color $N"
