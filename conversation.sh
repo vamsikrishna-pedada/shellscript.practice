082aefa30342c27e2e4b9299dc355254a588f74c
Vamsi <vamsikrishnapedada19@gmail.com>
2026-02-01 17:34:45 +0530

    new file addded

--- conversation.sh
+++ conversation.sh
@@ -7,4 +7,4 @@ echo "Jameer:: yes vinod, am learning DevSecOps with AWS in JoinDevops"
 echo "Vinod:: is it Jameer? did you check online before taking this course like the instructor & way of teaching"
 echo "Jameer:: yeah i checked everything and finally i joined here."
 echo "Vinod:: Ok Jameer, All the best & Hope you will get  new job soon"
-echo "Jameer:: Thanks Vinod"
+echo "Jameer:: Thanks Vinod"s
