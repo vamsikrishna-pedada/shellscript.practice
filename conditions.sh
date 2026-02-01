2243b97bb39a0d8812af9a556a3f3a9bcfdcc8f6
Vamsi <vamsikrishnapedada19@gmail.com>
2026-02-01 18:00:10 +0530

    new file addded

--- conditions.sh
+++ conditions.sh
@@ -2,12 +2,12 @@
 
 Number=65
 
-if [$NUMBER -gt 65 ]; then
+if [ $NUMBER -gt 65 ]; then
     echo "Given number:: $NUMBER is greater than 65"
-elif [$NUMBER -eq 65 ]; then
-    echo "Given number:: $NUMBER is equal to 65"
-elif [$NUMBER -ne 65 ]; then
-    echo "Given number:: $NUMBER is not equal to 65"
+elif [ $NUMBER -eq 65 ]; then
+    echo "Given number: $NUMBER is equal to 65"
+elif [ $NUMBER -ne 65 ]; then
+    echo "Given number: $NUMBER is not equal to 65"
 else 
-    echo "Given number:: $NUMBER is less than 65 "
+    echo "Given number: $NUMBER is less than 65 "
 fi
