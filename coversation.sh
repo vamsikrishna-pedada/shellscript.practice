91c466be4e0f3c4b878dd882aa81b0e9ecec0e57
Vamsi <vamsikrishnapedada19@gmail.com>
2026-02-01 10:48:30 +0530

    new file

---
+++ coversation.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "Vinod:: Hi Jameer, How are you doing?"
+echo "Jameer:: H iVInod, I'm doing good. What about you vinod?"
+echo "Vinod:: Yeah going good Jameer. Are you taking an course for upskilling?"
+echo "Jameer:: yes vinod, am learning DevSecOps with AWS in JoinDevops"
+echo "Vinod:: is it Jameer? did you check online before taking this course like the instructor & way of teaching"
+echo "Jameer:: yeah i checked everything and finally i joined here."
+echo "Vinod:: Ok Jameer, All the best & Hope you will get  new job soon"
+echo "Jameer:: Thanks Vinod"
