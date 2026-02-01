40b7d4019ea2d8967b98a835a9dbcf340dd7cdec
Vamsi <vamsikrishnapedada19@gmail.com>
2026-02-01 17:12:23 +0530

    new file addded

---
+++ variables01.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "All args passed to script: $@"
+echo "Number of vars passed to script: $#"
+echo "Script name: $0"
+echo "Present directory: $PWD"
+echo "Who is running: $USER"
+echo "Home directory of current user: $HOME"
+echo "PID of this script: $$"
+sleep 100 &
+echo "PID of recently executed background process: $!"
+echo "All args passed to script: $*"
