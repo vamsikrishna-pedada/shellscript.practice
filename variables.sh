f8e1aa2425bcfc193b9622f5a3c90b385267d941
Vamsi <vamsikrishnapedada19@gmail.com>
2026-02-01 11:21:25 +0530

    new file addded

---
+++ variables.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+PERSON1=Vinod
+PERSON2=Jameer
+echo "$PERSON1:: Hi $PERSON2, How are you doing?"
+echo "$PERSON2:: Hi $PERSON1, I'm doing good. What about you $PERSON1?"
+echo "$PERSON1:: Yeah going good $PERSON2. Are you taking an course for upskilling?"
+echo "$PERSON2:: yes $PERSON1, am learning DevSecOps with AWS in JoinDevops"
+echo "$PERSON1:: is it $PERSON2? did you check online before taking this course like the instructor & way of teaching"
+echo "$PERSON2:: yeah i checked everything and finally i joined here."
+echo "$PERSON1:: Ok $PERSON2, All the best & Hope you will get  new job soon"
+echo "$PERSON2:: Thanks $PERSON1"
